8442e371616435e86adce02c08e6314c099dee20
Mohanbarman <mohanbarman9711@gmail.com>
2019-11-30 18:23:03 +0530

    addeding history functionality

--- applyfilter.py
+++ applyfilter.py
@@ -1,17 +1,17 @@
 from PIL import Image, ImageFilter
 
-def applyFilter(background):
+def applyFilter(background, last_image_number):
 
     # For background wallpaper
     filter_ = Image.open('src/filter.png')
     background_image = Image.open(background)
     filtered = Image.blend(background_image, filter_, 0.6)
 
-    filtered.save('filtered.png')
+    filtered.save(f'{last_image_number}-filtered.png')
 
     # For lockscreen
     filter_ = Image.open('src/lock-filter.png')
     filtered = filtered.filter(ImageFilter.GaussianBlur(radius=10))
     filtered.paste(filter_, (0,0), filter_)
 
-    filtered.save('filtered-lock.png')
+    filtered.save(f'{last_image_number}-filtered-lock.png')
